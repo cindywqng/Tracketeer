97126c23891b4b0d442abb7a7860d19476469c1b
cindywqng <cindyx.wang2016@gmail.com>
2025-07-09 20:27:15 -0400

    Create TracketeerApp.swift

---
+++ TracketeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TracketeerApp.swift
+//  Tracketeer
+//
+//  Created by Student on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TracketeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeScreen()
+        }
+    }
+}
